638b8296041d6d0a5d9e849307a851bfa1f4708e
Pavel Baranov <pb@smarthead.ru>
2020-09-24 12:57:06 +0300

    оптимизация

--- index.js
+++ index.js
@@ -3,28 +3,29 @@ function func(text, entities) {
 	let countOffset = 0;
 	
 	entities.forEach((el, index) => {
-		if (el.type === "bold") {
-			parts.push(text.slice(countOffset, el.offset));
-			parts.push(`**${text.slice(el.offset, el.offset + el.length)}**`);
-			countOffset = el.offset + el.length;
-		}
-		if (el.type === "italic") {
-			parts.push(text.slice(countOffset, el.offset));
-			parts.push(`__${text.slice(el.offset, el.offset + el.length)}__`);
-			countOffset = el.offset + el.length;
-		}
-		if (el.type === "text_link") {
-			parts.push(text.slice(countOffset, el.offset));
-			parts.push(`[${text.slice(el.offset, el.offset + el.length)}](${el.url})`);
-			countOffset = el.offset + el.length + el.url.length;
-		}
 		
-		 if (index === entities.length - 1) {
+		parts.push(text.slice(countOffset, el.offset));
+		
+		checkType(el, "bold", "**")
+		checkType(el, "italic", "__")
+		checkType(el, "text_link", "[", "]", el.url)
+		
+		if (index === entities.length - 1) {
 			parts.push(text.slice(countOffset, text.length));
 		}
 		
 	})
 
+	function checkType(obj, type, char, char2 = "", url = "") {
+			if (obj.type === type) {
+			parts.push(`${char}${text.slice(obj.offset, obj.offset + obj.length)}${char2?char2:char}` 
+			+ `${url ? "(" + url + ")" : ""}`);
+			countOffset = obj.offset + obj.length
+		}
+	}
+	
+	
+	
 	return parts.join("");
 }
 
